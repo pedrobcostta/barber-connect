7f4831595d557b661757af0f888c40ffb14c6fa3
[dyad] <joaopedrob837@icloud.com>
2025-09-22 20:09:41 -0300

    [dyad] Fixing a critical syntax error - wrote 1 file(s)

--- src/components/auth/RegisterForm.tsx
+++ src/components/auth/RegisterForm.tsx
@@ -133,7 +133,7 @@ export function RegisterForm({ onSwitchToLogin }: RegisterFormProps) {
             <FormItem><FormLabel style={{ color: "#0D131A" }}>E-mail de Acesso</FormLabel><FormControl><Input placeholder="seu@email.com" {...field} /></FormControl><FormMessage /></FormItem>
           )}/>
           <FormField control={form.control} name="password" render={({ field }) => (
-            <FormItem><FormLabel style={{ color: "#0D131A" }}>Crie uma Senha Forte</FormLabel><FormControl><Input type="password" {...field} /></FormControl><FormMessage /></FormMessage /></FormItem>
+            <FormItem><FormLabel style={{ color: "#0D131A" }}>Crie uma Senha Forte</FormLabel><FormControl><Input type="password" {...field} /></FormControl><FormMessage /></FormItem>
           )}/>
           <FormField control={form.control} name="confirmPassword" render={({ field }) => (
             <FormItem><FormLabel style={{ color: "#0D131A" }}>Confirme sua Senha</FormLabel><FormControl><Input type="password" {...field} /></FormControl><FormMessage /></FormItem>
